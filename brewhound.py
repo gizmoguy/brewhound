c8b2354dee46e4ab3d935430c8e670b09abb2f2c
Brad Cowie <brad@gizmoguy.net.nz>
2016-03-28 17:47:51 +1300

    Initial commit.

---
+++ brewhound.py
@@ -0,0 +1,64 @@
+from flask import Flask, Response, render_template
+app = Flask(__name__)
+
+from bs4 import BeautifulSoup
+import urllib, json
+
+@app.route('/')
+def api_root():
+    return render_template('index.html')
+
+@app.route('/taplist/<locationid>')
+def api_taplist(locationid):
+    try:
+        locid = int(locationid)
+    except:
+        return json.dumps({'error': 'Bad location ID'})
+
+    url = 'http://brewhound.nz/embedlocation.php?locationID={:d}'.format(locid)
+
+    r = urllib.urlopen(url)
+    soup = BeautifulSoup(r)
+
+    locdiv     = soup.find('div', {'class': 'locationDetails'})
+    name       = locdiv.h3.text
+    contactdiv = locdiv.find_all('div', {'class': 'contactDetails'})
+
+    contactdeets = []
+    for deet in contactdiv:
+        contactdeets.append(deet.text)
+
+    location = {
+        'name': name,
+        'contact': contactdeets
+        }
+
+    listings = soup.find('ul', id='listings')
+    taplist = []
+    for li in listings.findAll('li'):
+        brewdiv = li.find('div', {'class': ['itemBrewery', 'phoneVisible']})
+        beerdiv = li.find('div', {'class': 'itemTitle'})
+        metadiv = li.find_all('div', {'class': 'itemAlcohol'})
+        logodiv = li.find('div', {'class': 'itemLogo'})
+
+        brewery = brewdiv.a.text
+        beer    = beerdiv.a.text
+        style   = metadiv[0].text
+        abv     = metadiv[1].text
+        badge   = "http://brewhound.nz/" + urllib.quote(logodiv.a.img['src'])
+
+        tapdict = {
+            'brewery': brewery,
+            'beer':    beer,
+            'style':   style,
+            'abv':     abv,
+            'badge':   badge
+            }
+        taplist.append(tapdict)
+
+    ret  = json.dumps({'location': location, 'taplist': taplist})
+    resp = Response(ret, status=200, mimetype='application/json')
+    return resp
+
+if __name__ == '__main__':
+    app.run()
